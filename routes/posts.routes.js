9b2d26c6e719d50509f2342e509176f23d8e1024
karthickram286 <karthick.r@zohocorp.com>
2019-12-29 09:31:31 +0530

    post routes

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -50,4 +50,74 @@ router.post('/add', [auth, createPostValidation], async (req, res) => {
     }
 });
 
+/**
+ * @route   GET /api/post/getall
+ * @desc    Get all posts
+ * @access  Private
+ */
+router.get('/getall', auth, async (req, res) => {
+    try {
+        // Getting posts and sorting them based on updated time
+        const posts = await Post.find().sort({ updatedAt: -1 });
+
+        if (!posts) {
+            return res.status(404).json({ msg: 'No posts found' });
+        }
+        return res.status(200).json(posts);
+    } catch (err) {
+        logger.error(err.message);
+        return res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route   GET /api/post/get/:post_id
+ * @desc    Get a post by Id
+ * @access  Private
+ */
+router.get('/get/:post_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        return res.status(200).json(post);
+    } catch (err) {
+        logger.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        return res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route   DELETE /api/post/delete/:post_id
+ * @desc    Deletes a post for an Id
+ * @access  Private
+ */
+router.delete('/delete/:post_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.post_id);
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        // Checking if the user of the post matches the username of the requested user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await post.remove();
+        return res.status(200).json({ msg: 'Post deleted' });
+    } catch (err) {
+        logger.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        return res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
