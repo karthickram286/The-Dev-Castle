aaca1fc64ce2bca59b2064ec410fea43f8d0dda5
karthickram286 <karthick.r@zohocorp.com>
2020-01-01 21:45:25 +0530

    redux boilerplate code

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,31 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import './App.css';
 
+// Redux imports
+import { Provider } from 'react-redux';
+import store from './store';
+
+
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Fragment>
-          <Navbar />
-          <Route exact path="/" component={ Landing } />
-          <section className="container">
-            <Switch>
-              <Route exact path="/register" component={ Register } />
-              <Route exact path="/login" component={ Login } />
-            </Switch>
-          </section>
-      </Fragment>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <Fragment>
+            <Navbar />
+            <Route exact path="/" component={ Landing } />
+            <section className="container">
+              <Switch>
+                <Route exact path="/register" component={ Register } />
+                <Route exact path="/login" component={ Login } />
+              </Switch>
+            </section>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
